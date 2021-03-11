895a9e60625379d6949efb4512e7f9364b48caff
Александр Хохлачёв <demcy.meizu@gmail.com>
2021-03-11 17:04:26 +0200

    initial commit

---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TodoListItem from '../todo-list-item';
+import './todo-list.css';
+
+const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
+    const items = todos.map((item) => {
+        const { id, ...itemProps } = item;
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem 
+                    {...itemProps} 
+                    onDeleted={() => onDeleted(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleDone={() => onToggleDone(id)} />
+            </li>
+        );
+    });
+    return (
+        <ul className="list-group todo-list">
+            {items}
+        </ul>
+    );
+};
+
+export default TodoList;
